bd3876890b1768ecfa7f8cdb59188271f5094804
Jaron <jaron.has@upcmail.nl>
2022-01-17 10:22:24 +0100

    Added the more complicated way of tracking time

--- model.py
+++ model.py
@@ -20,6 +20,8 @@ class Model:
         self.mortality_rate = mortality_rate
 
         self.network = network
+        self.t = 0
+        self.finished = False
 
         # we keep track of the status of each node (person)
         nx.set_node_attributes(self.network, "S", "status")
@@ -29,8 +31,8 @@ class Model:
         This function sets the status of every node in the network back to susceptible
         """
         nx.set_node_attributes(self.network, "S", "status")
-        self.infecteds = np.array([0])
-        self.network.nodes[0]["status"] = "I"
+        self.t = 0
+        self.finished = False
 
     def infect(self, number):
         """
@@ -44,7 +46,8 @@ class Model:
 
     def step(self):
         """
-        This function simulates one timestep of the model.
+        This function simulates one timestep of the model, using the simple method
+        of computing probabilities for an entire day.
         """
 
         # We change the status of the nodes at the very end of one time step. This
@@ -65,7 +68,10 @@ class Model:
                     if self.network.nodes[neighbour]["status"] == "I":
                         infected_neighbours += 1
 
-                exposed_prob = (infected_neighbours / len(self.network.edges(node))) * self.infection_rate
+                exposed_prob = 0
+                if len(self.network.edges(node)):
+                    exposed_prob = (infected_neighbours / len(self.network.edges(node))) * self.infection_rate
+
                 if np.random.choice([0,1], p=[1 - exposed_prob, exposed_prob]):
                     newly_exposed = np.append(newly_exposed, [node])
 
@@ -91,9 +97,62 @@ class Model:
         for node in newly_dead:
             self.network.nodes[node]["status"] = "F"
 
+        self.t += 1
 
+    def step_complicated(self):
+        """
+        This function performs one timestep of the model using the complicated method.
+        """
 
+        if self.finished:
+            return
 
+        # These two values will help us to decide a time when the next event will happen.
+        r = np.random.uniform()
+
+        # we start with elements in the list to make the appends work, there might be a nicer way to do this.
+        probabilities = np.array([0])
+        events = np.array([(0,0)])
+        for node in self.network.nodes:
+            # If the current node is susceptible it has a chance to be exposed to the virus via
+            # its neighbours. The model this is based on also includes a probability to be exposed
+            # via any other person in the network. We could add this later (or not).
+            if self.network.nodes[node]["status"] == "S":
+                infected_neighbours = 0
+                for edge in self.network.edges(node):
+                    neighbour = find_other_node(edge, node)
+                    if self.network.nodes[neighbour]["status"] == "I":
+                        infected_neighbours += 1
+
+                exposed_prob = 0
+                if len(self.network.edges(node)):
+                    exposed_prob = (infected_neighbours / len(self.network.edges(node))) * self.infection_rate
+
+                probabilities = np.append(probabilities, [exposed_prob])
+                events = np.append(events, [(node, "E")], axis=0)
+
+            # If the current node is exposed, every day it has a chance to become infected/infectuous.
+            elif self.network.nodes[node]["status"] == "E":
+                probabilities = np.append(probabilities, [self.incubation_period])
+                events = np.append(events, [(node, "I")], axis=0)
+
+            # If the current node is infected, every day it has a chance to recover and a chance to die.
+            elif self.network.nodes[node]["status"] == "I":
+                probabilities = np.append(probabilities, [self.recovery_rate])
+                events = np.append(events, [(node, "R")], axis=0)
+
+                probabilities = np.append(probabilities, [self.mortality_rate])
+                events = np.append(events, [(node, "F")], axis=0)
+
+        if np.sum(probabilities)>0:
+            probability_of_event = 1 - np.prod(1 - probabilities)
+            time_to_next_event = (1 / probability_of_event) * np.log(1 / r)
+            self.t += time_to_next_event
+
+            event = np.random.choice(np.arange(len(events)), p=probabilities/np.sum(probabilities))
+            self.network.nodes[int(events[event][0])]["status"] = events[event][1]
+        else:
+            self.finished = True
 
     def get_infecteds(self):
         return [node for node in self.network.nodes if self.network.nodes[node]["status"] == "I"]
@@ -110,19 +169,23 @@ class Model:
     def get_deads(self):
         return [node for node in self.network.nodes if self.network.nodes[node]["status"] == "F"]
 
+    def get_time(self):
+        return self.t
+
 
 if __name__ == "__main__":
     test_network = nx.watts_strogatz_graph(100, 6, 0.05, seed=None)
 
     test_model = Model(test_network, 0.5, 0.5, 0.125, 0.125)
     test_model. infect(1)
-    for i in range(1000):
+    for i in range(100):
+        print(test_model.get_time())
         print(test_model.get_susceptibles())
         print(test_model.get_exposeds())
         print(test_model.get_infecteds())
         print(test_model.get_deads())
         print(test_model.get_recovereds(),"\n")
-        test_model.step()
+        test_model.step_complicated()
     #print(test_model.get_susceptibles())
 
 
