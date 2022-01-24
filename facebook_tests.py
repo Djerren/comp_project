c0c6d4959f99c5014a2900ce1b4b8d9a94cc43f4
koenjules <koen.hoeberechts@gmail.com>
2022-01-24 18:32:28 +0100

    meer stats

--- facebook_tests.py
+++ facebook_tests.py
@@ -43,39 +43,39 @@ def test(iterations, infection_rate, incubation_period, infection_time, vaccinat
 
 def print_stats(age_file, degree_file):
     age_stats = open(age_file)
-    lines = 0
-    age_infected = 0
-    age_dead = 0
+    lines, age_time, age_infected, age_dead = [0] * 4
     for line in age_stats:
         lines += 1
         line_split = line.split(";")
+        age_time += int(line_split[1])
         age_infected += int(line_split[2])
         age_dead += int(line_split[3])
     age_stats.close()
+    age_time /= lines
     age_infected /= lines
     age_dead /= lines
 
     degree_stats = open(degree_file)
-    lines = 0
-    degree_infected = 0
-    degree_dead = 0
+    lines, degree_time, degree_infected, degree_dead = [0] * 4
     for line in degree_stats:
         lines += 1
         line_split = line.split(";")
+        degree_time += int(line_split[1])
         degree_infected += int(line_split[2])
         degree_dead += int(line_split[3])
     degree_stats.close()
+    degree_time /= lines
     degree_infected /= lines
     degree_dead /= lines
 
-    print("age:")
+    print(f"age: {age_time} steps")
     print(f"infected: {round(age_infected, 2)}, dead: {round(age_dead, 2)}")
-    print("degree:")
+    print(f"degree: {degree_time} steps")
     print(f"infected: {round(degree_infected, 2)}, dead: {round(degree_dead, 2)}")
 
 
 def main():
-    #test(20, 0.5, 0.2, 7, 100)
+    # test(50, 0.5, 0.2, 7, 100)
     print_stats("stats/fb_age_0.5_0.2_7_40.txt", "stats/fb_degree_0.5_0.2_7_40.txt")
 
 if __name__ == "__main__":
