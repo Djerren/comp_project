83e5d4a906968f1fb4171dcb7ca90a43b05bd181
Jaron <jaron.has@upcmail.nl>
2022-01-29 15:06:23 +0100

    added verifications

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import networkx as nx
 from code.helper_functions import facebook_network
 from code.model import Model
 from code.visualise import draw_graph_simple, get_average, graph_infection_rate, print_network, boxplot, print_mean
+from code.verification import verify_incubation_period_and_infection_time, verify_infect, verify_infection_rate, verify_vaccination_rate
 
 def main():
     # Model(network, infection_rate, incubation_rate, recovery_rate, mortality_rate, ages, vaccination_method, vaccination_rate, vaccination_start)
@@ -101,6 +102,7 @@ if __name__ == "__main__":
             # "stats/fb_random_0.5_0.2_7_40.txt", "stats/fb_none_0.5_0.2_7_40.txt")
     # graph_infection_rate("death")
     # graph_infection_rate("infections")
+    """
     file = open(f"stats/fb_age_1.0_0.20_7_40.txt")
     average_old = get_average(file, 3)
     file.close()
@@ -111,3 +113,9 @@ if __name__ == "__main__":
 
     print(average_old)
     print(average_new)
+    """
+    sf_network = nx.barabasi_albert_graph(1000, 7)
+    verify_infect(1000, [250, 500, 750])
+    verify_infection_rate([0.25, 0.5, 0.75, 1], [250, 500, 750], 1000)
+    verify_incubation_period_and_infection_time(sf_network, 10, 4/7, 7)
+    verify_vaccination_rate(sf_network, 10, [10, 100, 200])
