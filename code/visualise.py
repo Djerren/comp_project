c1bd2ecbd5c6e18928e313b5726be04fe2bed055
Jaron <jaron.has@upcmail.nl>
2022-02-01 10:37:31 +0100

    whitespace

--- code/visualise.py
+++ code/visualise.py
@@ -92,9 +92,9 @@ def get_average(file, index):
     for line in file:
         line_split = line.split(";")
         list += [int(line_split[index])]
-    
+
     return np.mean(list), np.std(list)
-   
+
 def compare_methods(data, parameter):
     if data == "time":
         index = 1
@@ -102,7 +102,7 @@ def compare_methods(data, parameter):
         index = 2
     else:
         index = 3
-    
+
     if parameter == "infection_rate":
         parameters = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
         file_names = []
@@ -133,7 +133,7 @@ def compare_methods(data, parameter):
         file_names = []
         for p in parameters:
             file_names.append(f"1.0_5_10_25_0.05_{format(p, '.2f')}.txt")
-    
+
     age_avg, degree_avg = [[],[]]
     age_std, degree_std = [[],[]]
 
@@ -146,7 +146,7 @@ def compare_methods(data, parameter):
         avg, std = get_average(file, index)
         degree_avg += [avg]
         degree_std += [std]
-    
+
     plt.subplot(121)
     plt.ylim(0,max(max(age_avg),max(degree_avg))+max(max(age_std),max(degree_std)))
     plt.errorbar(parameters, age_avg, yerr=age_std, label="age", marker="o", markersize=3, linestyle="none", elinewidth=1)
@@ -162,7 +162,7 @@ def graph_methods(data, parameter):
         index = 2
     else:
         index = 3
-    
+
     if parameter == "infection_rate":
         parameters = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
         file_names = []
@@ -193,7 +193,7 @@ def graph_methods(data, parameter):
         file_names = []
         for p in parameters:
             file_names.append(f"1.0_5_10_25_0.05_{format(p, '.2f')}.txt")
-    
+
     none_avg, random_avg, age_avg, degree_avg = [[] for i in range(4)]
     none_std, random_std, age_std, degree_std = [[] for i in range(4)]
 
@@ -214,7 +214,7 @@ def graph_methods(data, parameter):
         avg, std = get_average(file, index)
         degree_avg += [avg]
         degree_std += [std]
-    
+
     diff = 0.05 * (parameters[1] - parameters[0])
     parameters1 = [p - diff for p in parameters]
     parameters2 = [p + diff for p in parameters]
@@ -224,4 +224,4 @@ def graph_methods(data, parameter):
     plt.errorbar(parameters2, age_avg, yerr=age_std, label="age", marker="o", markersize=3, linestyle="none", elinewidth=1)
     plt.errorbar(parameters3, degree_avg, yerr=degree_std, label="degree", markersize=3, marker="o", linestyle="none", elinewidth=1)
     plt.legend()
-    plt.show() 
+    plt.show()
