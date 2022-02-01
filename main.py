c13649b5858c668ee5252cf3f976e9bffdfce819
Jaron <jaron.has@upcmail.nl>
2022-02-01 11:41:55 +0100

    standardization of creating figures

--- main.py
+++ main.py
@@ -2,19 +2,49 @@ import networkx as nx
 from code.visualise import draw_graph_simple, get_average, graph_methods, compare_methods, print_network, boxplot
 from code.verification import verify_incubation_period_and_infection_time, verify_infect, verify_infection_rate, verify_vaccination_rate
 
-if __name__ == "__main__":
-    # boxplot("stats/fb_age_0.5_0.2_7_40.txt", "stats/fb_degree_0.5_0.2_7_40.txt",
-
-
-    # compare_methods("infections", "infection_rate")
-    # compare_methods("deaths", "infection_rate")
-    # compare_methods("time", "infection_rate")
-
+def poster_figures():
+    """
+    This function creates the figures in the poster and the report from pre-computed data.
+    """
     graph_methods("infections", "infection_rate")
     graph_methods("deaths", "infection_rate")
-    # sf_network = nx.barabasi_albert_graph(1000, 7)
-    # verify_infect(1000, [250, 500, 750])
-    # verify_infection_rate([0.25, 0.5, 0.75, 1], [250, 500, 750], 1000)
-    # verify_incubation_period_and_infection_time(sf_network, 10, 5, 7)
-    # verify_vaccination_rate(sf_network, 10, [10, 100, 200])
+
+def alternative_figures(parameter):
+    """
+    This function creates figures similar to the poster figures, but for other parameters as well.
+    Figures are created using pre-computed data
+    """
+    graph_methods("infections", parameter)
+    graph_methods("deaths", parameter)
+    graph_methods("time", parameter)
+
+def compare_figures(parameter):
+    """
+    With this code, degree and age based vaccination can be compared in different metrics
+    (infections, deaths and time) for changes in different parameters. Figures are created
+    using pre-computed data.
+    """
+    compare_methods("infections", parameter)
+    compare_methods("deats", parameter)
+    compare_methods("time", parameter)
+
+def verifications():
+    """
+    This code runs a few verification tests on the base model, we verified the more complicated additions
+    just by checking if the results made sense.
+    """
+    sf_network = nx.barabasi_albert_graph(1000, 7)
+    verify_infect(1000, [250, 500, 750])
+    verify_infection_rate([0.25, 0.5, 0.75, 1], [250, 500, 750], 1000)
+    verify_incubation_period_and_infection_time(sf_network, 10, 5, 7)
+    verify_vaccination_rate(sf_network, 10, [10, 100, 200])
+
+
+if __name__ == "__main__":
+    poster_figures()
+    # alternative_figures("incubation_period")
+    # compare_figures("incubation_period")
+    # verifications()
+
+
 
